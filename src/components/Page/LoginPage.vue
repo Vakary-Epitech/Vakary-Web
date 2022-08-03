c0c41ba01e4eab2fef561027911197e204e2a52a
Pierre Brun <pierrebrun@pierres-macbook-pro.home>
2022-08-03 14:46:17 +0200

    ADD google connexion

--- src/components/Page/LoginPage.vue
+++ src/components/Page/LoginPage.vue
@@ -1,9 +1,102 @@
 <template>
-  <div>Login Page</div>
+  <div>Connexion</div>
+  <button v-if="!pro" v-on:click="pro = false" style="background: #959595"> Connexion Standard</button>
+  <button v-if="!pro" v-on:click="pro = true"> Connexion Professionelle</button>
+  <button v-if="pro" v-on:click="pro = false" > Connexion Standard</button>
+  <button v-if="pro" v-on:click="pro = true"  style="background: #959595"> Connexion Professionelle</button>
+  <form>
+     <label v-show="pro">Code d'authentification:</label>
+    <input type="code"   v-show="pro" required v-model="code">
+    <label>Email ou Nom d'utilisateur:</label>
+    <input type="email"   required v-model="email">
+
+    <label>Mot de passe:</label>
+    <input type="password"   required v-model="password">
+    <label>Mot de passe oublie ??</label>
+  </form>
+
+  <button> Connexion</button>
+  <br>
+  <label>ou</label>
+  <button > Inscription</button>
+  <br>
+
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      pro: false
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    /* text-transform: uppercase; */
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    background-color: withe;;
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border-bottom: 1px solid #ddd;
+    border-radius: 200px;
+    border: 2px solid rgb(192, 150, 40);
+    color: #959595;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+  button {
+    background: white;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: black;
+    border-radius: 200px;
+    border: 2px solid rgb(192, 150, 40);
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/store.js'
+// import GAuth from 'vue3-google-oauth2'
+// let gauthClient = '832869712029-8lvrg9k2t6fp7hkc4tis632u0nh5hlcc.apps.googleusercontent.com'
 
+
+// const gAuthOptions = { clientId: gauthClient, scope: 'email', prompt: 'consent', fetch_basic_profile: false }
+// app.use(GAuth, gAuthOptions)
 const app = createApp(App);
+app.mount('#app');
 app.use(router);
 app.use(store);
-app.mount('#app');
