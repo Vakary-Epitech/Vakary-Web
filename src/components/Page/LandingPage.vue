9e4dcf18ba9dda67f4d92aba2d9d63afeae8bad1
Pierre FRICKER <pierre.fricker@epitech.eu>
2022-10-06 12:55:40 +0900

    [ADD] topBAr

--- src/components/Page/LandingPage.vue
+++ src/components/Page/LandingPage.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
+    <top-bar class="col-12"/>
     Landing Page
   </div>
 </template>
 
 <script>
+  import TopBar from '../Shared/TopBar.vue';
+  export default {
+    components: {
+      'top-bar': TopBar
+    }
+  }
 </script>
 
 <style scoped>
---
+++ src/components/Shared/TopBar.vue
@@ -0,0 +1,110 @@
+<template>
+        <div class="row addBorder mt-3">
+            <div class="col-2 mb-3">
+                <img @click="(openMobilePage)" class="logo" src="../../assets/Logo_vect.svg">
+            </div>
+            <div class="col-2 my-auto">
+                <button class="default-color to-page" @click="(openMobilePage)">Application Web</button>
+            </div>
+
+            <div class="col-2 my-auto">
+                <button class="default-color to-page" @click="(openMobilePage)">Application Mobile</button>
+            </div>
+
+            <div class="col-2 my-auto">
+                <button class="default-color to-page" @click="(openMobilePage)">VIP</button>
+            </div>
+
+            <div class="col-2 my-auto">
+                <button class="default-color connexion-button" @click="(openLoginPage)">Connexion</button>
+                
+            </div>
+
+            <div class="col-2 my-auto">
+                <button class="register-button" @click="(openMobilePage)">Inscription</button>
+            </div>
+
+        </div>
+</template>
+
+<style>
+    #app {
+        margin-top: 0;
+    }
+
+    .addBorder {
+        border-bottom: 1px solid #000000;
+    }
+
+    .row{
+        margin-left: 0 !important;
+    }
+
+    .logo {
+        width: auto;
+        height: 50px;
+        cursor: pointer;
+    }
+
+    .default-color {
+        color: #000642;
+    }
+
+    .default-color:hover {
+        color: #C09628;
+    }
+
+    .to-page {
+        border: none;
+        background-color: #fff;
+    }
+
+    .register-button {
+        background-color: #000642;
+        color: #C09628;
+        border: 3px #000642 solid;
+        border-radius: 20px;
+    }
+
+    .register-button:hover {
+        background-color: #C09628;
+        color: #000642;
+        border: 3px #C09628 solid;
+        font-size: 20px;
+    }
+
+    .connexion-button {
+        background-color: #fff;
+        border: 3px #000642 solid;
+        border-radius: 20px;
+    }
+
+    .connexion-button:hover {
+        font-size: 20px;
+        border: 3px #C09628 solid;
+    }
+
+</style>
+
+<script>
+    export default {
+        name: 'TopBar',
+        methods: {
+            openLandingPage() {
+            this.$router.push("/");
+            },
+            openLoginPage() {
+            this.$router.push("/loginPage");
+            },
+            openMapPage() {
+            this.$router.push("/mapPage");
+            },
+            openProfilePage() {
+            this.$router.push("/profilePage");
+            },
+            openMobilePage() {
+            this.$router.push("/mobilePage");
+            },
+        },
+    };
+</script>
