53f3d042626d046c4eb37820725cc0c5f289f681
TomW <tom.wederich@epitech.eu>
2022-10-20 22:31:42 +0200

    [ADD] design for standard and professional connexion

--- src/components/Page/LoginPage.vue
+++ src/components/Page/LoginPage.vue
@@ -1,41 +1,40 @@
 <template>
-  <div class="login">
-    <label>Connexion</label>
-    <button v-if="!pro" v-on:click="pro = false" class="buttonSelectLogin"> Connexion Standard</button>
-    <button v-if="!pro" v-on:click="pro = true"> Connexion Professionelle</button>
-    <button v-if="pro" v-on:click="pro = false" > Connexion Standard</button>
-    <button v-if="pro" v-on:click="pro = true"  class="buttonSelectLogin"> Connexion Professionelle</button>
-    <form v-show="pro">
-      <label>Code d'authentification:</label>
-      <input type="code" required v-model="code">
-      <label>Code authentification oublie ?</label>
-      <label>Email ou Nom d'utilisateur:</label>
-      <input type="email" required v-model="email">
-      <label>Mot de passe:</label>
-      <input type="password" required v-model="password">
-      <label>Mot de passe oublie ?</label>
-    </form>
-  
-    <form v-show="!pro">
-      <label>Email ou Nom d'utilisateur:</label>
-      <input type="email" required v-model="email">
-      <label>Mot de passe:</label>
-      <input type="password" required v-model="password">
-      <button @click="(openForgetPassword)">Mot de passe oublie ?</button>
-    </form>
-  
-    <button> Connexion</button>
-    <button>
-      <img src="@/assets/google.png" width="100" height="100">
-    </button>
-    <label>ou</label>
-    <button @click="(openSelectAccount)">Inscription</button>
+  <div class="login elementHorizontalyCentered">
+    <img src="@/assets/Logo_vect.svg" class="logoSize elementBasicMargin elementHorizontalyCentered" />
+    <label class="elementBasicMargin">Sign in to Vakary</label>
+    <div class="elementHorizontalyCentered">
+      <form v-show="pro" class="loginPageInformationContainer">
+        <label>Code d'authentification:</label>
+        <input type="code" required v-model="code">
+        <label>Code authentification oublie ?</label>
+        <label>Email ou Nom d'utilisateur:</label>
+        <input type="email" required v-model="email">
+        <label>Mot de passe:</label>
+        <input type="password" required v-model="password">
+        <label>Mot de passe oublie ?</label>
+      </form>
+
+      <form v-show="!pro" class="loginPageInformationContainer">
+        <label>Email ou Nom d'utilisateur:</label>
+        <input type="email" required v-model="email">
+        <label>Mot de passe:</label>
+        <input type="password" required v-model="password">
+        <button class="basicVakaryButton"> Connexion</button>
+        <button class="basicVakaryButton" @click="(openForgetPassword)">Mot de passe oublie ?</button>
+      </form>
+    </div>
+    <div class="elementHorizontalyCentered loginPageInscriptionContainer">
+      <label class="newToText">New to Vakary ? </label>
+      <button class="InscriptionButton blueVakaryButton" @click="(openSelectAccount)">Inscription</button>
+    </div>
+    <button class="basicVakaryButton" v-if="!pro" v-on:click="pro = true"> Connexion Professionelle</button>
+    <button class="basicVakaryButton" v-if="pro" v-on:click="pro = false"> Connexion Standard</button>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       pro: false
     }
@@ -52,27 +51,30 @@ export default {
 </script>
 
 <style scoped>
+@import "@/components/Style/Button.scss";
 
-.buttonSelectLogin {
-  background: #959595;
+.elementBasicMargin {
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
+
+.newToText {
+  margin-top: 3px;
+  margin-left: 5px;
+}
+
+.InscriptionButton {
+  margin: auto 5px;
+}
+
 .login {
+  display: flex;
   background-color: var(--background-color-primary);
-}
-
-.login button {
-  display: flex; 
-  flex-direction: column;
-  text-align: center; 
+  text-align: center;
+  justify-content: center;
   align-items: center;
-  background: white;
-  margin: 10px auto;
-  border: 0;
-  color: black;
-  background-color: var(--background-color-secondary);
-  color: var(--text-primary-color);
-  border: 2px solid rgb(192, 150, 40);
-  border-radius: 20px;
+  flex-direction: column;
+  transform: translate(0%, 18%);
 }
 
 .login input {
@@ -85,4 +87,27 @@ export default {
   color: var(--text-primary-color);
 }
 
+.loginPageInscriptionContainer {
+  display: flex;
+  justify-content: center;
+  border: 1px solid #000642;
+  border-radius: 15px;
+  padding: 5px;
+  width: 300px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.loginPageInformationContainer {
+  display: flex;
+  background-color: var(--background-color-primary);
+  text-align: center;
+  flex-direction: column;
+  border: 4px solid rgb(192, 150, 40);
+  border-radius: 15px;
+  padding: 5px;
+  width: 300px;
+}
+
+;
 </style>
