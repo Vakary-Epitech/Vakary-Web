b0079107efe51f6639bec2e691ee15002fe01e3c
TomW <tom.wederich@epitech.eu>
2022-10-21 02:09:06 +0200

    [DELETE] delete test from deleted file

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,8 +1,6 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import CreateGroup from '@/components/UI/CreateGroup.vue'
 import Group from '@/components/Page/Group.vue'
-import VakaryHome from '@/components/Page/VakaryHome.vue'
-import VakaryWeb from '@/components/Page/VakaryWeb.vue'
 
 describe('CreateGroup.vue', () => {
   it('renders props.msg when passed', () => {
@@ -20,25 +18,5 @@ describe('Group.vue', () => {
 
     expect(wrapper.vm.showModal).toEqual(false)
     expect(wrapper.vm.createGroup).toEqual(false)
-  })
-})
-
-describe('VakaryHome.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'Prêt à découvrir votre monde ?'
-    const wrapper = shallowMount(VakaryHome, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toContain(msg)
-  })
-})
-
-describe('VakaryWeb.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'Vos vacances selon vos envies'
-    const wrapper = shallowMount(VakaryWeb, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toContain(msg)
   })
 })
