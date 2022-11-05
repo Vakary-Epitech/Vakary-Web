503153dcb315f0dc8f946853c69faeb513c35547
Pierre FRICKER <pierre.fricker@epitech.eu>
2022-11-05 17:03:36 +0900

    [FIX] fixing tests

--- tests/unit/GroupePage.spec.js
+++ tests/unit/GroupePage.spec.js
@@ -1,22 +1,33 @@
 import { mount, shallowMount } from '@vue/test-utils'
-import CreateGroup from '@/components/UI/CreateGroup.vue'
 import GroupPage from '@/components/Page/GroupPage.vue'
 
-describe('CreateGroup.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'Sauvegarder'
-    const wrapper = shallowMount(CreateGroup, {
-      props: { msg }
+describe("GroupPage", () => {
+  it("renders successfully", () => {
+    const wrapper = mount(GroupPage, {
+      global: {
+        mocks: {
+          $t: (msg) => msg
+        }
+      }
     })
-    expect(wrapper.text()).toContain(msg)
-  })
-})
-
-describe('GroupPage.vue', () => {
-  it('renders props.msg when passed', () => {
-    const wrapper = mount(GroupPage)
-
-    expect(wrapper.vm.showModal).toEqual(false)
-    expect(wrapper.vm.createGroup).toEqual(false)
-  })
+  }),
+  it("getComponent GroupPage", () => {
+    const wrapper = shallowMount(GroupPage, {
+        global: {
+          mocks: {
+            $t: (msg) => msg
+          }
+        }
+      })
+      wrapper.getComponent(GroupPage)
+  }),
+  it('isVisible', () => {
+    const wrapper = shallowMount(GroupPage, {
+        global: {
+            mocks: {
+                $t: (msg) => msg
+            }
+        }
+      })
+    })
 })
