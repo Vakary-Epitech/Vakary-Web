a6a702cc4ea26191be86331c3e1ead61724c5a79
TomW <tom.wederich@epitech.eu>
2023-05-02 10:02:16 -0400

    [ADD] adding back a consolelog for error

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -13,6 +13,7 @@ const userStore = {
     },
     mutations: {
         UPDATE_USER_LOCATION(state, coords) {
+            console.log("User Coords:", coords.latitude, coords.longitude);
             state.currentUserLocation.latitude = 48.8610061;
             state.currentUserLocation.longitude = 2.3359302999999727;
         },
