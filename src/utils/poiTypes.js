e456c3c4d4b9f4c35356fe475297ebe9bb5ade70
exy-tek <pierre.fricker@epitech.eu>
2023-05-15 23:06:35 +0900

    [CHANGES] adding the new system with enums from the back [locals.json] new translations added for later, not used atm [poiTypes.js] enums from the back [itineraryModal.vue] adding the new system of checkbox to send to the back, everything working as intended

---
+++ src/utils/poiTypes.js
@@ -0,0 +1,62 @@
+export const InterestPointTypeAccommodation = {
+    ACCOMMODATION: "Accommodation",
+    GUESTHOUSE: "GuestHouse",
+    HOTEL: "Hotel",
+    HOLIDAYRESORT: "Holiday Resort",
+    WINERY: "Winery",
+    SELF_CATERING_ACCOMMODATION: "Self Catering Accommodation",
+    LODGING_BUSINESS: "Lodging Business",
+};
+
+export const InterestPointTypeEatOrDrink = {
+    RESTAURANT: "Restaurant",
+    FASTFOOD: "Fast Food",
+    CAFE: "Cafe",
+    BISTRO: "Bistro",
+    PUB: "Pub",
+    NIGHTCLUB: "Nightclub",
+};
+
+export const InterestPointTypeShop = {
+    STORE: "Club",
+    CRAFTSMAN_SHOP: "Craftsman Shop",
+    BUSINESS_PLACE: "Business Place",
+};
+
+export const InterestPointTypeSport = {
+    STADIUM: "Stadium",
+    ARENA: "Arena",
+    SPORT_PLACE: "Sport Place",
+    MINIGOLF: "Minigolf",
+    SWIMMINGPOOL: "Swimming Pool",
+    BOWLING: "Bowling",
+};
+
+export const InterestPointTypePlaceToVisit = {
+    AMUSEMENT_PARK: "Amusement Park",
+    CHURCH: "Church",
+    CINEMA: "Cinema",
+    CULTURAL_SITE: "Cultural Site",
+    CYCLING_TOUR: "Cycling Tour",
+    LANDFORM: "Landform",
+    NATURAL_HERITAGE: "Natural Heritage",
+    REMARKABLE_BUILDING: "Remarkable Building",
+    THEATER: "Theater",
+    LIBRARY: "Library",
+    CITY_HERITAGE: "City Heritage",
+    MUSEUM: "Museum",
+    CASTLE: "Castle",
+    RIVER: "River",
+    LEISURE_COMPLEX: "Leisure Complex",
+    ROAD_TOUR: "Road Tour",
+    PARK_GARDEN: "Park Garden",
+    WALKING_TOUR: "Walking Tour",
+};
+
+export const InterestPointTypeGroup = {
+    ACCOMMODATION: "Accommodation",
+    RESTAURATION: "Restauration",
+    PLACE_TO_VISIT: "Place to visit",
+    SHOP: "Shopping",
+    SPORT: "Sport",
+};
