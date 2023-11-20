0fd0069bc7002716cf881e4abbe828b90218cc53
Pierre Fricker <p.fricker@immopad.com>
2023-11-20 15:17:48 +0100

    add likes / apk / fix translation / FAQ

--- src/components/Page/PresentationVakaryTeam.vue
+++ src/components/Page/PresentationVakaryTeam.vue
@@ -112,6 +112,27 @@
             <hr class="separationBar">
         </div>
         <RoadMap style="margin-top: 40px;" />
+        <div class="barClass">
+            <hr class="separationBar">
+            <span style="font-size: calc(15px + 0.8vw); margin: auto">{{ $t('presentationVakaryTeam.faq.title') }}</span>
+            <hr class="separationBar">
+        </div>
+        <div class="container my-3">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item" v-for="index in faqNumber" :key="index">
+                    <h2 class="accordion-header" :id="'heading' + index">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse' + index" :aria-expanded="false" :aria-controls="'collapse' + index">
+                        {{ getTranslation('presentationVakaryTeam.faq.faq', index) }}
+                        </button>
+                    </h2>
+                    <div :id="'collapse' + index" class="accordion-collapse collapse" :aria-labelledby="'heading' + index" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        {{ getTranslation('presentationVakaryTeam.faq.ans', index) }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -126,6 +147,16 @@ export default {
     setup() {
 
     },
+    data() {
+        return {
+            faqNumber: 13,
+        }
+    },
+    methods: {
+        getTranslation(key, index) {
+            return this.$t(key + index);
+        }
+    }
 }
 </script>
 
