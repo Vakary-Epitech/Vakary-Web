52f03063ba00e91647b180a55d1745eed1b26c1a
RassGo <62484150+RassGo@users.noreply.github.com>
2024-01-02 06:36:41 +0100

    FIX indentation

--- src/components/UI/itineraryModal.vue
+++ src/components/UI/itineraryModal.vue
@@ -228,50 +228,50 @@ export default {
             const selectedProfile = this.$store.state.profileStore.userTravelProfile[this.profileActiveIndex];
 
             if (selectedProfile && selectedProfile.InterestPointTypes) {
-                this.$store.dispatch("addItinerary", {
-                city: this.city,
-                availableTime: duration,
-                budget: this.budget,
-                nbPeople: this.people,
-                nbChild: this.children,
-                typeResearchLocations: selectedProfile.InterestPointTypes,
-                handicapAccess: false,
-                }).then(() => {
-                this.$store.dispatch("getItinerary").then(() => {
-                    this.loading = false;
-                    this.$emit("goBackToItineraryDropdown");
-                    this.$emit("itineraryCreated");
-                });
-                }).catch((error) => {
-                this.loading = false;
-                this.error = error?.response?.data;
-                });
-            } else {
-                this.$store.dispatch("getCityTypeByName", this.city ).then((cityType) => {
-                this.$store.dispatch("addItinerary", {
+                    this.$store.dispatch("addItinerary", {
                     city: this.city,
                     availableTime: duration,
                     budget: this.budget,
                     nbPeople: this.people,
                     nbChild: this.children,
-                    typeResearchLocations: cityType, 
+                    typeResearchLocations: selectedProfile.InterestPointTypes,
                     handicapAccess: false,
                 }).then(() => {
                     this.$store.dispatch("getItinerary").then(() => {
-                    this.loading = false;
-                    this.$emit("goBackToItineraryDropdown");
-                    this.$emit("itineraryCreated");
+                        this.loading = false;
+                        this.$emit("goBackToItineraryDropdown");
+                        this.$emit("itineraryCreated");
                     });
                 }).catch((error) => {
                     this.loading = false;
                     this.error = error?.response?.data;
                 });
+            } else {
+                this.$store.dispatch("getCityTypeByName", this.city ).then((cityType) => {
+                    this.$store.dispatch("addItinerary", {
+                        city: this.city,
+                        availableTime: duration,
+                        budget: this.budget,
+                        nbPeople: this.people,
+                        nbChild: this.children,
+                        typeResearchLocations: cityType, 
+                        handicapAccess: false,
+                    }).then(() => {
+                        this.$store.dispatch("getItinerary").then(() => {
+                        this.loading = false;
+                        this.$emit("goBackToItineraryDropdown");
+                        this.$emit("itineraryCreated");
+                        });
+                    }).catch((error) => {
+                        this.loading = false;
+                        this.error = error?.response?.data;
+                    });
                 }).catch((error) => {
-                this.loading = false;
-                this.error = error?.response?.data;
+                    this.loading = false;
+                    this.error = error?.response?.data;
                 });
             }
-            },
+        },
     }
 }
 </script>
